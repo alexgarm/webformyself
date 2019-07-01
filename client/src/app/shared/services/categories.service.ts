e8914afd180fb208f58bea19d26c8543ca5403b5
alexgarm.github.io <git@github.com:alexgarm/alexgarm.github.io.git>
2019-07-01 13:04:13 +0300

    1.7.19

--- client/src/app/shared/services/categories.service.ts
+++ client/src/app/shared/services/categories.service.ts
@@ -5,14 +5,20 @@ import { Observable } from 'rxjs';
 
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
-export class CategoriesService{
+export class CategoriesService {
   constructor(private http: HttpClient) {
 
   }
-  fetch(): Observable<Category[]>{
-       return this.http.get<Category[]>('/api/category')
+  fetch(): Observable<Category[]> {
+       return this.http.get<Category[]>('/api/category');
+
+  }
+
+
+  getById(id: string): Observable<Category>{
+   return this.http.get<Category>(`/api/category/${id}`);
   }
 
 }
